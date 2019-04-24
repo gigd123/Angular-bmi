7b80dfd93778be0a055be7eeb98fd24643fe8e13
gigd123 <gigdleo@gmail.com>
2019-04-24 23:00:36 +0800

    set bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'bmi-todolist',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-bmi';
+
 }
